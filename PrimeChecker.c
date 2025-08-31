5dfff2c6c5616282d7e0ddd930b90ff49260e689
ddavis2843 <91286107+ddavis2843@users.noreply.github.com>
2025-08-31 21:39:22 +0000

    Labeled each part of code

--- PrimeChecker.c
+++ PrimeChecker.c
@@ -4,14 +4,14 @@
 
 int main() {
 
-    int given_number;
+    int given_number; // 1. Define an integer variable as given_number.
 
     int is_prime = 1; 
 
     printf("Enter a value: ");
 
  
-    scanf("%d", &given_number);
+    scanf("%d", &given_number); // 2. Use the C function scanf to read the integer variable given_number.
 
 
     if (given_number <= 1) {
@@ -24,7 +24,7 @@ int main() {
 
         for (int i = 2; i <= sqrt(given_number); i++) {
 
-            if (given_number % i == 0) {
+            if (given_number % i == 0)  { // 3. Use the conditional statement to find out given_number is prime or not prime.
 
                 is_prime = 0; 
 
@@ -44,6 +44,6 @@ int main() {
         
         printf("The number is not prime.\n");
     }
-
+// 4. Print the final output as The number is prime or The number is not prime.
     return 0; 
 }
